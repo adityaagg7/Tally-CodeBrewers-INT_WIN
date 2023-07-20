ff1f1862f271dd0c6fa4040d6fc801bc6a6159cb
adityaagg7 <79642133+adityaagg7@users.noreply.github.com>
2023-07-20 21:10:01 +0530

    fix: Remove hardcoded path

--- efficient_delete_find_pipe_rm.py
+++ efficient_delete_find_pipe_rm.py
@@ -1,24 +1,22 @@
 import subprocess
 import time
+import os
 
 
 def delete_files_in_directory(directory):
     """Delete all files in the specified directory using find and xargs."""
     try:
         start_time = time.time()
-        # subprocess.run(["find", directory, "-type", "f", "-print0", "|", "xargs", "-0", "rm", "-f"])
         command = f"find {directory} -type f -print0 | xargs -0 rm -f"
         subprocess.run(command, shell=True)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(
-            f"Successfully deleted all files in the directory in {elapsed_time:.2f} seconds."
-        )
+
     except Exception as e:
         print(f"Failed to delete files: {e}")
 
 
 def main():
-    directory_to_delete = "/home/manav/Workspaces/vscode/cli/test2"  # Replace with the directory where you want to delete files
+    directory_to_delete = input("Enter path for directory of leave empty for Home ")
+    if directory_to_delete == "":
+        directory_to_delete = os.path.expanduser("~")
 
     delete_files_in_directory(directory_to_delete)
