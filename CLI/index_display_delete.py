96467d01a5681c11db81f608f855e788a4b93e3c
adityaagg7 <79642133+adityaagg7@users.noreply.github.com>
2023-07-22 00:40:17 +0530

    undo:removed multiple deletion tries

--- CLI/index_display_delete.py
+++ CLI/index_display_delete.py
@@ -10,45 +10,45 @@ def main(files):
         table.add_row([i,m[i][1],m[i][0]],divider=True)
         i+=1        
 
-    while 1:
-        print(table)
-        inp=input("\nEnter the comma-separated indices of the files to be deleted or leave empty to exit:   ") 
-        
-        if len(inp)==0: 
-            break
-        if(inp=='all'):
-            for path,size in files:
-                os.remove(path)
-                print("Deleted ",path,"\n")
-            print("Deleted all Files\n")
-            break
-                
-        indices=inp.split(',')
-        for index in indices:
 
-            if index.count('-')>0:
-                index1=int(index.split('-')[0])
-                index2=int(index.split('-')[1])
-                for i in range(index1,index2+1):
-                    if os.path.exists(m[int(i)][0]):
-                        os.remove(m[int(i)][0])
-                        table.del_row(int(i))
-                        print("Deleted ",m[int(i)][0],"\n")
-                    else:
-                        print(f"File {m[int(i)][0]} not found \n")
-            else:
-                if os.path.exists(m[int(index)][0]):
-                        os.remove(m[int(index)][0])
-                        table.del_row(int(index))
-                        print("Deleted ",m[int(index)][0],"\n")
+    print(table)
+    inp=input("\nEnter the comma-separated indices of the files to be deleted or leave empty to exit:   ") 
+    
+    if len(inp)==0: 
+        return
+    if(inp=='all'):
+        for path,size in files:
+            os.remove(path)
+            print("Deleted ",path,"\n")
+        print("Deleted all Files\n")
+        return 
+            
+    indices=inp.split(',')
+    for index in indices:
+
+        if index.count('-')>0:
+            index1=int(index.split('-')[0])
+            index2=int(index.split('-')[1])
+            for i in range(index1,index2+1):1
+                if os.path.exists(m[int(i)][0]):
+                    os.remove(m[int(i)][0])
+                    table.del_row(int(i))
+                    print("Deleted ",m[int(i)][0],"\n")
                 else:
-                    print(f"File {m[int(index)][0]} not found \n")
-                
+                    print(f"File {m[int(i)][0]} not found \n")
+        else:
+            if os.path.exists(m[int(index)][0]):
+                    os.remove(m[int(index)][0])
+                    table.del_row(int(index))
+                    print("Deleted ",m[int(index)][0],"\n")
+            else:
+                print(f"File {m[int(index)][0]} not found \n")
             
+        
 
         
             
-main([('/Users/sunidhiaggarwal/Documents/GitHub/Tally-CodeBrewers-INT_WIN/tesat/testssfd', 24),('/home/gsuare/Desktop/hi/h1.txt', 77)])
+# main([('/Users/sunidhiaggarwal/Documents/GitHub/Tally-CodeBrewers-INT_WIN/tesat/testssfd', 24),('/home/gsuare/Desktop/hi/h1.txt', 77)])
         
         
         
