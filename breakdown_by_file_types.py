dc92355059b1c7b2c35ab752727f75fd18d6c3dd
Manav Sidana <manavsidana00@gmail.com>
2023-07-21 14:25:30 +0530

    fix: made breakdown_by_file_types much more efficient

--- breakdown_by_file_types.py
+++ breakdown_by_file_types.py
@@ -2,6 +2,15 @@ import os
 import subprocess
 from tqdm import tqdm
 
+image_set={"jpg","tif","tiff","eps", "jpeg", "png", "gif", "bmp", "raw", "cr2","nef","orf","sr2","webp","svg"}
+video_set={"mp4", "avi", "mkv", "mov", "wmv"}
+document_set={"pdf", "doc", "docx", "txt", "ppt", "pptx"}
+audio_set={"mp3", "wav", "ogg", "flac", "aac"}
+compressed_set={"zip", "rar", "tar", "gz", "7z"}
+code_set={"py", "java", "cpp", "h", "html", "css", "js", "json"}
+
+
+
 def get_size_formatted(size_bytes):
     # Function to convert bytes to a human-readable format (e.g., KB, MB, GB, etc.)
     size_kb = size_bytes / 1024
@@ -13,18 +22,33 @@ def get_size_formatted(size_bytes):
     size_gb = size_mb / 1024
     return f"{size_gb:.2f}GB"
 
-def get_total_size(directory, type, extensions):
-    total_size_bytes = 0
-    with tqdm(total=len(extensions), ascii=" #" ,desc=f"Searching {type}", unit="type") as pbar:
-        for extension in extensions:
-            cmd = f'find {directory} -type f -iname "*.{extension}" -exec stat -c %s {{}} +'
-            result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-            file_sizes_str = result.stdout.strip()
-            if file_sizes_str:
-                file_sizes = map(int, file_sizes_str.split('\n'))
-                total_size_bytes += sum(file_sizes)
+def get_total_size(directory, map):
+    total_size=0
+    cmd = f"find {directory} -type f -iname '*.*'"
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    file_str = result.stdout.strip().split('\n')
+    with tqdm(total=len(file_str), ascii=" #" ,desc=f"Searching: ", unit=" files") as pbar:
+        for file in file_str:
+            file_extension=file.split(".")[-1]
+            try:
+                file_size=os.path.getsize(file)
+                if file_extension in image_set:
+                    map["image_sizes"]+=file_size
+                elif file_extension in video_set:
+                    map["video_sizes"]+=file_size
+                elif file_extension in document_set:
+                    map["document_sizes"]+=file_size
+                elif file_extension in audio_set:
+                    map["audio_sizes"]+=file_size
+                elif file_extension in compressed_set:
+                    map["compressed_sizes"]+=file_size
+                elif file_extension in code_set:
+                    map["code_sizes"]+=file_size
+                total_size+=file_size
+            except:
+                pass
             pbar.update(1)
-    return total_size_bytes
+    return total_size
 
 def get_total_size_of_directory(directory):
     cmd="du -s "+directory
@@ -34,39 +58,29 @@ def get_total_size_of_directory(directory):
     return total_size
 
 def main():
-    image_extensions = ["jpg","tif","tiff","eps", "jpeg", "png", "gif", "bmp", "raw", "cr2","nef","orf","sr2","webp","svg"]
-    video_extensions = ["mp4", "avi", "mkv", "mov", "wmv"]
-    document_extensions = ["pdf", "doc", "docx", "txt", "ppt", "pptx"]
-    audio_extensions = ["mp3", "wav", "ogg", "flac", "aac"]
-    compressed_extensions = ["zip", "rar", "tar", "gz", "7z"]
-    code_extensions = ["py", "java", "cpp", "h", "html", "css", "js"]
+    map={"image_sizes":0,"video_sizes":0,"document_sizes":0,"audio_sizes":0,"compressed_sizes":0,"code_sizes":0}
 
     current_directory = input("Enter the directory you want to search in\n(leave blank for home directory)\n(enter . for searching current directory): ")
     if current_directory=="":
         current_directory='/home/manav/'
+    if current_directory==".":
+        current_directory=os.getcwd()
 
-    extensions = image_extensions + video_extensions + document_extensions + audio_extensions + compressed_extensions + code_extensions
-    total_image_size_bytes = get_total_size(current_directory, "images", image_extensions)
-    total_video_size_bytes = get_total_size(current_directory, "videos", video_extensions)
-    total_document_size_bytes = get_total_size(current_directory, "documents", document_extensions)
-    total_audio_size_bytes = get_total_size(current_directory, "audio", audio_extensions)
-    total_compressed_size_bytes = get_total_size(current_directory, "archives",  compressed_extensions)
-    total_code_size_bytes = get_total_size(current_directory, "programming files", code_extensions)
-    total_image_size = get_size_formatted(total_image_size_bytes)
-    total_video_size = get_size_formatted(total_video_size_bytes)
-    total_document_size = get_size_formatted(total_document_size_bytes)
-    total_audio_size = get_size_formatted(total_audio_size_bytes)
-    total_compressed_size = get_size_formatted(total_compressed_size_bytes)
-    total_code_size = get_size_formatted(total_code_size_bytes)
-    total_extensions_size = (
-        total_image_size_bytes + total_video_size_bytes +
-        total_document_size_bytes + total_audio_size_bytes +
-        total_compressed_size_bytes + total_code_size_bytes
-    )
+    total_extensions_size=get_total_size(current_directory, map)
+
+    total_image_size = get_size_formatted(map["image_sizes"])
+    total_video_size = get_size_formatted(map["video_sizes"])
+    total_document_size = get_size_formatted(map["document_sizes"])
+    total_audio_size = get_size_formatted(map["audio_sizes"])
+    total_compressed_size = get_size_formatted(map["compressed_sizes"])
+    total_code_size = get_size_formatted(map["code_sizes"])
+    
+
     total_directory_size = get_total_size_of_directory(current_directory)
     total_directory_size=int(total_directory_size)*1024
     other_files_size=total_directory_size-total_extensions_size
     other_files_size=get_size_formatted(other_files_size)
+
     print(f"images: {total_image_size}")
     print(f"videos: {total_video_size}")
     print(f"documents: {total_document_size}")
